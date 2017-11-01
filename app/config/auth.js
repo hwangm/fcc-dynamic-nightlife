3b8f79c1cd8122f9c06bfaae05253237b9b9f142
Matt Hwang <hw4ng_m@yahoo.com>
2017-11-01 18:43:55 +0000

    Remove github/twitter auth configs

--- app/config/auth.js
+++ app/config/auth.js
@@ -1,16 +1,6 @@
 'use strict';
 
 module.exports = {
-	'githubAuth': {
-		'clientID': process.env.GITHUB_KEY,
-		'clientSecret': process.env.GITHUB_SECRET,
-		'callbackURL': process.env.APP_URL + 'auth/github/callback'
-	},
-	'twitterAuth': {
-		'consumerKey': process.env.TWITTER_KEY,
-		'consumerSecret': process.env.TWITTER_SECRET,
-		'callbackURL': process.env.APP_URL + 'auth/twitter/callback'
-	},
 	'googleAuth': {
 		'consumerKey': process.env.GOOGLE_KEY,
 		'consumerSecret': process.env.GOOGLE_SECRET,
